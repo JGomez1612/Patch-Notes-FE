93214b679d04565f3fb3a1a97f3141e65df35c95
JGomez1612 <jostgomez1612@gmail.com>
2025-10-15 23:46:07 -0400

    updated review page and tested backend connection.

--- src/pages/ReviewsPage.jsx
+++ src/pages/ReviewsPage.jsx
@@ -1,9 +1,42 @@
-export default function ReviewsPage() {
-    return (
-        <>
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function HomePage(){
+    const [review, setReview] = useState(null);
+
+    useEffect(() => {
+        const getReview = async () => {
+            try {
+                const res = await axios.get('http://localhost:3000/api/review/');
+
+                setReview(res.data);
+            } catch (err) {
+                console.err(err.message);
+            }
+        };
+
+        getReview();
+    }, []);
+
+    const loading = () => {
+        return <h1>Loading...</h1>
+    };
+
+    const loaded = () => {
+        return (
             <div>
                 <h1>Reviews</h1>
+                {review.map((r, i) => (
+                    <div key={i} className='reviewCard'>
+                        <h2>{r.title}</h2>
+                        <p>User: {r.userId.username}</p>
+                        <p>Game: {r.gameId}</p>
+                        <p>Review: {r.body}</p>
+                        <p>Rating: {r.rating}</p>
+                    </div>
+                ))};
             </div>
-        </>
-    )
+        );
+    };
+    return review ? loaded() : loading();
 }
