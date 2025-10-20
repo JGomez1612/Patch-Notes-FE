1698c53ab0101eef0c75408052461adb0bed08c7
JGomez1612 <jostgomez1612@gmail.com>
2025-10-20 15:01:32 -0400

    created login and registration forms and auth page.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import NavBar from './components/NavBar/NavBar';
 
 // Pages
 import HomePage from './pages/HomePage/HomePage';
+import AuthPage from './pages/AuthPage/AuthPage';
 import UserPage from './pages/UserPage/UserPage';
 import GamesPage from './pages/GamesPage/GamesPage';
 import ReviewsPage from './pages/ReviewsPage/ReviewsPage';
@@ -18,6 +19,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
         <Route path="/user" element={<UserPage />} />
         <Route path="/games" element={<GamesPage />} />
         <Route path="/reviews" element={<ReviewsPage />} />
