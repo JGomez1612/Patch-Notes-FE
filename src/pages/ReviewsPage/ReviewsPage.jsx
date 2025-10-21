3ccf847f63dd320b9dd8ea8b488b2dd2480b9719
JGomez1612 <jostgomez1612@gmail.com>
2025-10-21 19:41:39 -0400

    added images to reviews in reviews page.

--- src/pages/ReviewsPage/ReviewsPage.jsx
+++ src/pages/ReviewsPage/ReviewsPage.jsx
@@ -1,15 +1,29 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-export default function ReviewPage(){
+export default function ReviewPage() {
     const [review, setReview] = useState(null);
 
     useEffect(() => {
         const getReview = async () => {
             try {
                 const res = await axios.get('http://localhost:3000/api/review/');
+                const reviews = res.data;
 
-                setReview(res.data);
+                const reviewsWithImages = [];
+
+                for (const review of reviews) {
+                    const gameRes = await axios.get(
+                        `http://localhost:3000/api/game/${review.gameId}`
+                    );
+                    reviewsWithImages.push({
+                        ...review,
+                        gameImage: gameRes.data.background_image,
+                        gameName: gameRes.data.name,
+                    });
+                }
+
+                setReview(reviewsWithImages);
             } catch (err) {
                 console.err(err.message);
             }
@@ -26,13 +40,13 @@ export default function ReviewPage(){
         return (
             <div>
                 <h1>Reviews</h1>
-                {review.map((r, i) => (
+                {review.map((review, i) => (
                     <div key={i} className='reviewCard'>
-                        <h2>{r.title}</h2>
-                        <p>User: {r.userId.username}</p>
-                        <p>Game: {r.gameId}</p>
-                        <p>Review: {r.body}</p>
-                        <p>Rating: {r.rating}</p>
+                        <h2>{review.title}</h2>
+                        <img src={review.gameImage} alt={review.title} width="250px" />
+                        <p>{review.userId.username} reviewed {review.gameName}</p>
+                        <p>Rating: {review.rating}/10</p>
+                        <p>Review: {review.body}</p>
                     </div>
                 ))};
             </div>
